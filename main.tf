3d6b56fa30d214a2b6e02ccd29228af29e266731
Jay Rodriguez <jay@boxboat.com>
2023-07-11 17:13:51 -0500

    chore: include ss path

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ resource "google_dialogflow_cx_agent" "full_agent" {
   avatar_uri                 = "https://cloud.google.com/_static/images/cloud/icons/favicons/onecloud/super_cloud.png"
   enable_stackdriver_logging = true
   enable_spell_correction    = true
-  # security_settings          = ""
+  security_settings          = "projects/boxboat-dev/locations/global/securitySettings/c1da59c7b3f06a75"
   speech_to_text_settings {
     enable_speech_adaptation = true
   }
