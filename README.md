df00a443ea7a2ad263b28afa6a50af0354d4a808
Jay Rodriguez <jay@boxboat.com>
2023-07-11 16:48:14 -0500

    chore: config

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # gcp-df-agent
+
+enable dlp api
--- main.tf
+++ main.tf
@@ -8,7 +8,9 @@ resource "google_dialogflow_cx_agent" "full_agent" {
   avatar_uri                 = "https://cloud.google.com/_static/images/cloud/icons/favicons/onecloud/super_cloud.png"
   enable_stackdriver_logging = true
   enable_spell_correction    = true
+  # security_settings          = ""
   speech_to_text_settings {
     enable_speech_adaptation = true
   }
 }
+
