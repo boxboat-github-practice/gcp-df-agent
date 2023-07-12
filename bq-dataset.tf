5e75bc93fad80bdbd7cf8ac5408d83a64c2887cf
Jay Rodriguez <jay@boxboat.com>
2023-07-12 11:28:35 -0500

    chore: remove sa

--- bq-dataset.tf
+++ bq-dataset.tf
@@ -12,15 +12,7 @@ resource "google_bigquery_dataset" "dataset" {
 
   access {
     role          = "OWNER"
-    user_by_email = google_service_account.bqowner.email
-  }
-
-  access {
-    role          = "READER"
     user_by_email = "jay@boxboat.com"
   }
 }
 
-resource "google_service_account" "bqowner" {
-  account_id = "113226688192813986706"
-}
