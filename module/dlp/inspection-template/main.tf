36ff1a64442ea2feaeff171e5aa6bfafa79e78c6
Jay Rodriguez <jay@boxboat.com>
2023-07-13 19:20:51 -0500

    x

--- module/dlp/inspection-template/main.tf
+++ module/dlp/inspection-template/main.tf
@@ -17,7 +17,7 @@ resource "google_data_loss_prevention_inspect_template" "inspection-template" {
 
   inspect_config {
     dynamic "info_types" {
-      for_each = local.info_type.name
+      for_each = local.info_type
       content {
         name = local.info_type["name"]
       }
