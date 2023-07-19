d77d853a2eeab657134252e7592aa02201812e19
Jay Rodriguez <jay@boxboat.com>
2023-07-19 12:11:20 -0500

    fix content block

--- module/alert-policy/main.tf
+++ module/alert-policy/main.tf
@@ -2,15 +2,17 @@ resource "google_monitoring_alert_policy" "alert_policy" {
   display_name = var.display_name
   combiner     = var.combiner
   dynamic conditions {
-    for_each     = var.conditions_list
-    display_name = conditions.value.cond_display_name
-    condition_threshold {
-      filter     = conditions.value.filter
-      duration   = conditions.value.duration
-      comparison = conditions.value.comparison
-      aggregations {
-        alignment_period   = conditions.value.alignment_period
-        per_series_aligner = conditions.value.per_series_aligner
+    for_each = var.conditions_list
+    content {
+      display_name = conditions.value.cond_display_name
+      condition_threshold {
+        filter     = conditions.value.filter
+        duration   = conditions.value.duration
+        comparison = conditions.value.comparison
+        aggregations {
+          alignment_period   = conditions.value.alignment_period
+          per_series_aligner = conditions.value.per_series_aligner
+        }
       }
     }
   }
