82d1c9b5030cc03e886e591e7478e716927435b4
Jay Rodriguez <jay@boxboat.com>
2023-07-19 15:53:11 -0500

    chore: add base vars to root

--- alert-policy.tf
+++ alert-policy.tf
@@ -1,7 +1,15 @@
 module "alert_policy" {
-  source          = "./module/alert-policy"
-  for_each        = var.alert_policy_map
-  display_name    = each.value.display_name
-  combiner        = each.value.combiner
-  conditions_list = each.value.conditions_list
+  source                = "./module/alert-policy"
+  for_each              = var.alert_policy_map
+  display_name          = each.value.display_name
+  combiner              = each.value.combiner
+  enabled               = each.value.enabled
+  notification_channels = each.value.notification_channels
+  # variable doc_content {}
+  # variable doc_mime_type {}
+  as_auto_close                     = each.value.as_auto_close
+  as_nrl_period                     = each.value.as_nrl_period
+  as_ncs_notification_channel_names = each.value.as_ncs_notification_channel_names
+  as_ncs_renotify_interval          = each.value.as_ncs_renotify_interval
+  conditions_list                   = each.value.conditions_list
 }
