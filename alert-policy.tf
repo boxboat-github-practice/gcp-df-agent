d8207c10ff7ef8dc365c30a629027e5882dfd24b
Jay Rodriguez <jay@boxboat.com>
2023-07-19 15:54:33 -0500

    add proj

--- alert-policy.tf
+++ alert-policy.tf
@@ -1,6 +1,7 @@
 module "alert_policy" {
   source                = "./module/alert-policy"
   for_each              = var.alert_policy_map
+  project               = each.value.project
   display_name          = each.value.display_name
   combiner              = each.value.combiner
   enabled               = each.value.enabled
