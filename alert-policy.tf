613bc3abaacb2a4c2b03ad2aa9809f8fc51a7bdc
Jay Rodriguez <jay@boxboat.com>
2023-07-19 17:56:45 -0500

    feel like policy should bump to root handling

--- alert-policy.tf
+++ alert-policy.tf
@@ -8,9 +8,9 @@ module "alert_policy" {
   notification_channels = each.value.notification_channels
   # variable doc_content {}
   # variable doc_mime_type {}
-  as_auto_close                     = each.value.as_auto_close
-  as_nrl_period                     = each.value.as_nrl_period
-  as_ncs_notification_channel_names = each.value.as_ncs_notification_channel_names
-  as_ncs_renotify_interval          = each.value.as_ncs_renotify_interval
-  conditions_list                   = each.value.conditions_list
+  as_auto_close = each.value.as_auto_close
+  as_nrl_period = each.value.as_nrl_period
+  # as_ncs_notification_channel_names = each.value.as_ncs_notification_channel_names
+  # as_ncs_renotify_interval          = each.value.as_ncs_renotify_interval
+  conditions_list = each.value.conditions_list
 }
